8244266d03b1c5b184098b988def14c246f69b72
Blake Wright <btdubbzzz@gmail.com>
2021-04-15 14:00:27 -0500

    started framework for checking time slots against current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 $("#currentDay").text('Today is ' + dayjs().format('dddd MMMM Do YYYY'))
+var currentTime = dayjs().unix()
+
+var todayEightAM = dayjs().set('hour', 8).set('minute', 0)
+console.log(todayEightAM.format('HHmm'))
+
+function handleFormColors() {
+    if (isTimeSlotPassed()) {
+        //color the block red
+    }
+}
+
+function isTimeSlotPassed(timeSlot) {
+    if (todayEightAM < parseInt(dayjs().get('hour'))) {
+        return true
+    }
+}
