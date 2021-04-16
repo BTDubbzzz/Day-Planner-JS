659011d4eb4bae0b2afb439e5c0640fd1e4b891d
BTDubbzzz <btdubbzzz@gmail.com>
2021-04-15 22:49:32 -0500

    finished form color rendering based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,85 @@
-$("#currentDay").text('Today is ' + dayjs().format('dddd MMMM Do YYYY'))
-var currentTime = dayjs().unix()
+$('#currentDay').text('Today is ' + dayjs().format('dddd MMMM Do YYYY'));
+$('#currentTime').text(
+	'The time is ' + dayjs().format('HH') + ':' + dayjs().format('ss')
+);
 
-var eightAMForm = $("8am-form-slot")
-var nineAMForm = $("9am-form-slot")
-var tenAMForm = $("10am-form-slot")
-var elevenAMForm = $("11am-form-slot")
-var twelvePMForm = $("12pm-form-slot")
-var onePMForm = $("1pm-form-slot")
-var twoPMForm = $("2pm-form-slot")
-var threePMForm = $("3pm-form-slot")
-var fourPMForm = $("4pm-form-slot")
-var fivePMForm = $("5pm-form-slot") 
+var eightAMFormEl = {
+	target: $('#eightam-form-slot'),
+	time: 8,
+};
+var nineAMFormEL = {
+	target: $('#nineam-form-slot'),
+	time: 9,
+};
+var tenAMFormEL = {
+	target: $('#tenam-form-slot'),
+	time: 10,
+};
+var elevenAMFormEL = {
+	target: $('#elevenam-form-slot'),
+	time: 11,
+};
+var twelvePMFormEL = {
+	target: $('#twelvepm-form-slot'),
+	time: 12,
+};
+var onePMFormEL = {
+	target: $('#onepm-form-slot'),
+	time: 13,
+};
+var twoPMFormEL = {
+	target: $('#twopm-form-slot'),
+	time: 14,
+};
+var threePMFormEL = {
+	target: $('#threepm-form-slot'),
+	time: 15,
+};
+var fourPMFormEL = {
+	target: $('#fourpm-form-slot'),
+	time: 16,
+};
+var fivePMFormEL = {
+	target: $('#fivepm-form-slot'),
+	time: 17,
+};
 
-var todayEightAM = dayjs().set('hour', 8).set('minute', 0)
-console.log(todayEightAM.format('HHmm'))
+var timeSlots = [
+	eightAMFormEl,
+	nineAMFormEL,
+	tenAMFormEL,
+	elevenAMFormEL,
+	twelvePMFormEL,
+	onePMFormEL,
+	twoPMFormEL,
+	threePMFormEL,
+	fourPMFormEL,
+	fivePMFormEL,
+];
 
-function handleFormColors() {
-    if (isTimeSlotPassed()) {
-        //color the block red
-    }
+var todayEightAM = dayjs().set('hour', 8).set('minute', 0);
+
+function handleFormColors(elementChecked) {
+	console.log(
+		'parseInt(dayjs().get("hour")) :>> ',
+		parseInt(dayjs().get('hour'))
+	);
+	console.log(
+		'parseInt(elementChecked.time) :>> ',
+		parseInt(elementChecked.time)
+	);
+	if (parseInt(dayjs().get('hour')) > parseInt(elementChecked.time)) {
+		elementChecked.target.children('.re-color').addClass('bg-danger');
+	} else if (parseInt(dayjs().get('hour')) === parseInt(elementChecked.time)) {
+		elementChecked.target.children('.re-color').addClass('bg-secondary');
+	} else if (parseInt(dayjs().get('hour')) < parseInt(elementChecked.time)) {
+		elementChecked.target.children('.re-color').addClass('bg-success');
+	}
 }
 
-function isTimeSlotPassed(timeSlot) {
-    if (todayEightAM < parseInt(dayjs().get('hour'))) {
-        return true
-    }
+function renderFormColors() {
+	for (let i = 0; i < timeSlots.length; i++) {
+		handleFormColors(timeSlots[i]);
+	}
 }
+renderFormColors();
